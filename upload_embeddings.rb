c121d62ddab577ce70bf9b0f0afc83bc843534fa
Fredrik Bränström <branstrom@gmail.com>
2023-09-13 22:11:43 +0200

    Refactor code to use Pinecone gem for uploading embeddings
    This commit refactors the code to use the Pinecone gem for uploading embeddings instead of making direct HTTP requests. The Pinecone API key is now retrieved from the system environment variable. The `upload_to_pinecone` method has been updated to accept a `client` parameter, which is an instance of the Pinecone client initialized with the API key. Embeddings are now upserted using the `upsert_item` method provided by the Pinecone gem. Error handling has also been improved to catch and display any exceptions that occur during vector upload.

--- upload_embeddings.rb
+++ upload_embeddings.rb
@@ -1,46 +1,31 @@
-require 'net/http'
-require 'uri'
-require 'json'
-require 'oj'
+require 'pinecone'
 
-# Placeholder Pinecone API key
-PINECONE_API_KEY = "YOUR_PINECONE_API_KEY"
-PINECONE_ENDPOINT = "https://api.pinecone.io/v1/vectors"
+# Use the Pinecone API key from the system environment variable
+PINECONE_API_KEY = ENV['PINECONE_API_KEY']
 
-def upload_to_pinecone(embeddings_data)
-  uri = URI(PINECONE_ENDPOINT)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
+# Initialize the Pinecone client
+pinecone_client = Pinecone::Client.new(api_key: PINECONE_API_KEY)
 
-  # Set up the request headers
-  headers = {
-    'Content-Type' => 'application/json',
-    'Authorization' => "Bearer #{PINECONE_API_KEY}"
-  }
-
+def upload_to_pinecone(embeddings_data, client)
   embeddings_data.each do |item|
-    request = Net::HTTP::Post.new(uri, headers)
-    request.body = Oj.dump({
-      namespace: "code_embeddings",
-      id: item['id'],
-      vector: item['embedding'],
-      metadata: item['metadata']
-    }, mode: :compat)
-
-    response = http.request(request)
-
-    # Handle the response
-    if response.code.to_i >= 400
-      puts "Error uploading vector with ID #{item['id']}: #{response.body}"
-    else
+    begin
+      client.upsert_item(
+        namespace: "code_embeddings",
+        id: item['id'],
+        vector: item['embedding'],
+        metadata: item['metadata']
+      )
       puts "Successfully uploaded vector with ID #{item['id']}"
+    rescue => e
+      puts "Error uploading vector with ID #{item['id']}: #{e.message}"
     end
   end
 end
 
 # Load the embeddings data from the file
 embeddings_file = "embeddings.json"
-embeddings_data = Oj.load(File.read(embeddings_file), symbol_keys: true)
+embeddings_data = Oj.load_file(embeddings_file, symbol_keys: true)
 
 # Upload the embeddings to Pinecone
-upload_to_pinecone(embeddings_data)
+upload_to_pinecone(embeddings_data, pinecone_client)
+
